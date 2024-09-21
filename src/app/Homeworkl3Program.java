4626fc49d588d4fb01a8021fc72a560abe6b9d94
Maksym2105 <maxdetrai02@gmail.com>
2024-09-21 11:06:51 +0300

    Add new system.out.println

--- src/app/Homeworkl3Program.java
+++ src/app/Homeworkl3Program.java
@@ -4,5 +4,6 @@ public class Homeworkl3Program {
 
     public static void main(String [] args){
         System.out.println("This is shop-online app");
+        System.out.println("Shop-online for customers");
     }
 }
